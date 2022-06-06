b645512ef47e93ea4b5de3c16b5780dfb494a142
pafonbu <pafonbu@gmail.com>
2022-06-06 17:12:42 -0400

    ADD: enviar notificaciones oneSignal y guardarlas en la mongo

--- api/models/NotificacionesPersonalizadas.js
+++ api/models/NotificacionesPersonalizadas.js
@@ -2,24 +2,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Deudas = mongoose.model(
-  "notificacion",
+  "notificaciones_personalizadas",
   new Schema(
     {
-      identificador: { type: String, required: true },
-      tipo: { type: String, required: true, enum: ["PAGARE"] },
-      codigoEstablecimiento: { type: String, required: true, enum: ["HRA"] },
-      correlativo: { type: Number, required: true },
+      correlativo: { type: String, required: true, unique: true },
+      idOneSignal: { type: String, required: true, unique: true, default: 1 },
+      tituloEs: { type: String, required: true },
+      mensajeEs: { type: String, required: true },
+      tituloEn: { type: String },
+      mensajeEn: { type: String },
       rutPaciente: { type: String, required: true },
-      fecha: { type: Date, required: true },
-      valor: { type: Number, required: true },
-      notificacion: { type: Number, required: true },
-      rutDeudor: { type: String, required: true },
-      nombreDeudor: { type: String, required: true },
+      estado: { type: String, required: true, default: "ENVIADA" },
+      fechaCreacion: { type: Date, required: true },
+      leida: { type: Boolean, default: false },
+      fijada: { type: Boolean, default: false },
+      codigoEstablecimiento: { type: String, required: true, enum: ["HRA"] },
       nombreEstablecimiento: {
         type: String,
         required: true,
         enum: ["Hospital Regional Antofagasta Dr. Leonardo Guzmán"],
       },
+      deletedAt: { type: Date, default: null },
     },
     { timestamps: true }
   )
