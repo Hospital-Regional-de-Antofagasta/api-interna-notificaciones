d539615933d13b0c1fcd4cffcb2ae5ab16cb7eeb
pafonbu <pafonbu@gmail.com>
2022-07-08 16:05:40 -0400

    log ci-cd

--- api/utils/oneSignalService.js
+++ api/utils/oneSignalService.js
@@ -6,6 +6,14 @@ const largeIcon = process.env.NOTIFICATION_LARGE_ICON;
 const urlOneSignal = process.env.ONE_SIGNAL_URL;
 
 exports.sendPushNotification = async (mensaje, titulo, idsSuscriptor) => {
+  console.log("sendPushNotification")
+  console.log("ONE_SIGNAL_APP_ID", oneSignalAppId)
+  console.log("ONE_SIGNAL_TOKEN", oneSignalToken)
+  console.log("NOTIFICATION_LARGE_ICON", largeIcon)
+  console.log("ONE_SIGNAL_URL", urlOneSignal)
+  console.log("mensaje", mensaje)
+  console.log("titulo", titulo)
+  console.log("idsSuscriptor", idsSuscriptor)
   const body = {
     app_id: oneSignalAppId,
     contents: { en: mensaje },
